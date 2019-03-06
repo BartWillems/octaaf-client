9728e347ccf11293a2128e9ca480e8a6900ecd06
Bart Willems <bwillems@protonmail.com>
2019-03-06 11:43:45 +0100

    No longer let the main function return errors Added structopt for command line arguments
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,51 @@
 #[macro_use]
 extern crate serde_derive;
+extern crate structopt;
 
 extern crate libc;
 extern crate reqwest;
 extern crate serde;
 
-use std::env;
+use structopt::StructOpt;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 struct Quote {
     #[serde(rename = "Quote")]
     quote: String,
     from: String,
 }
 
-fn main() -> Result<(), Box<std::error::Error>> {
+#[derive(StructOpt)]
+#[structopt(name = "basic")]
+struct Opt {
+    #[structopt(short = "f", long = "filter")]
+    filter: Option<String>,
+}
+
+fn main() {
     // Don't panic on broken pipes when writing to STDOUT
     unsafe {
         libc::signal(libc::SIGPIPE, libc::SIG_DFL);
     }
 
+    let opt = Opt::from_args();
+
+    let quote = fetch_quote(opt.filter);
+
+    match quote {
+        Ok(q) => println!("\"{}\"\n\t~{}", q.quote, q.from),
+        Err(e) => println!("Quote fetch error: {}", e),
+    }
+}
+
+fn fetch_quote(filter: Option<String>) -> Result<Quote, Box<std::error::Error>> {
     let client = reqwest::Client::new();
 
-    let res: Quote = client
+    let quote: Quote = client
         .get("http://188.166.33.109:8080/api/v1/kali/quote")
-        .query(&[("filter", get_filter())])
+        .query(&[("filter", filter.unwrap_or("".to_string()))])
         .send()?
         .json()?;
 
-    println!("\"{}\"\n\t~{}", res.quote, res.from);
-    Ok(())
-}
-
-fn get_filter() -> String {
-    let mut args: Vec<String> = env::args().collect();
-    let filter;
-    if args.len() > 1 {
-        filter = args.split_off(1).join(" ");
-    } else {
-        filter = "".to_string();
-    }
-    filter
+    Ok(quote)
 }

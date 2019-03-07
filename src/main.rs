a057da2b63148dcaeeabd9d49ec221b847d81529
Bart Willems <bwillems@protonmail.com>
2019-03-07 10:38:07 +0100

    Added support for presidential quotes
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate libc;
 extern crate reqwest;
 extern crate serde;
 
+use std::fs::File;
+use std::io;
+use std::process::Command;
 use structopt::StructOpt;
 
 #[derive(Deserialize)]
@@ -20,6 +23,9 @@ struct Quote {
 struct Opt {
     #[structopt(short = "f", long = "filter")]
     filter: Option<String>,
+    /// Quote type, either text or presidential
+    #[structopt(short = "t", long = "type", default_value = "text")]
+    quote_type: String,
 }
 
 fn main() {
@@ -30,11 +36,30 @@ fn main() {
 
     let opt = Opt::from_args();
 
-    let quote = fetch_quote(opt.filter);
+    match opt.quote_type.as_ref() {
+        "text" => {
+            let quote = fetch_quote(opt.filter);
 
-    match quote {
-        Ok(q) => println!("\"{}\"\n\t~{}", q.quote, q.from),
-        Err(e) => println!("Quote fetch error: {}", e),
+            match quote {
+                Ok(q) => println!("\"{}\"\n\t~{}", q.quote, q.from),
+                Err(e) => println!("Quote fetch error: {}", e),
+            }
+        }
+        "presidential" => {
+            let res = fetch_presidential_quote(opt.filter);
+            match res {
+                Ok(quote_file) => {
+                    Command::new("xdg-open")
+                        .arg(quote_file)
+                        .output()
+                        .expect("Failed to open presidential quote");
+                }
+                Err(e) => println!("Unable to fetch presidential quote: {}", e),
+            }
+        }
+        _ => {
+            println!("Invalid quote type given!");
+        }
     }
 }
 
@@ -48,4 +73,19 @@ fn fetch_quote(filter: Option<String>) -> Result<Quote, Box<std::error::Error>> 
         .json()?;
 
     Ok(quote)
+}
+
+fn fetch_presidential_quote(filter: Option<String>) -> Result<String, Box<std::error::Error>> {
+    let file_name = "/tmp/trumpie";
+    let params = [("filter", filter.unwrap_or_default())];
+
+    let mut resp = reqwest::Client::new()
+        .get("http://188.166.33.109:8080/api/v1/kali/quote/presidential")
+        .query(&params)
+        .send()?;
+
+    let mut out = File::create(file_name)?;
+    io::copy(&mut resp, &mut out)?;
+
+    Ok(file_name.to_string())
 }
